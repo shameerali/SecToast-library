277a56cdb0e9f7bf1d024e16471b528c29658042
Rhands <shameerali@iroidtechnologies.com>
2021-12-03 02:31:54 +0530

    ts

---
+++ app/src/main/java/com/luminuses/androlibrary/MainActivity.java
@@ -0,0 +1,17 @@
+package com.luminuses.androlibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.luminuses.toastlibrary.ToastMessage;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ToastMessage.s(getApplicationContext(), "hiiii");
+    }
+}
